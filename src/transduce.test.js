af6803b83b8e4c9fc1f65805a981065ac59bb9af
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-10 14:19:16 +0800

    RED/GREEN: transduce applies composed map and filter transducers to each element in the array

--- src/transduce.test.js
+++ src/transduce.test.js
@@ -2,11 +2,13 @@ import transduce from './transduce'
  
 describe('transduce', () => {
     const double = (num) => num * 2;
+    const isEven = (x) => x % 2 === 0;
+    const isGreaterThan5 = (x) => x > 5;
     const map = (func) => (step) => (accumulator, currentValue) => step(accumulator, func(currentValue));
     const filter = (predicate) => (step) => (a, c) => (predicate(c) ? step(a,c) : a)
-    const isEven = (x) => x % 2 === 0;
     const arrayReducer = (accumulator, currentValue) => accumulator.concat([currentValue]);
-    
+    const compose = (...transducers) => (step) =>
+      transducers.reduce((composedFn, fn) => fn(composedFn), step);
     it('applies a map transducer to double each element in the array', () => {
         const collection = [1,2,3];
         const initialValue = [];
@@ -22,6 +24,13 @@ describe('transduce', () => {
 
 
         expect(transduce(transducer, arrayReducer, initialValue, collection)).toEqual([2,4]);
+    });
+    it('applies composed map and filter transducers to each element in the array', () => {
+        const collection = [1,2,3,4,5];
+        const initialValue = [];
+        const transducer = compose(filter(isGreaterThan5), map(double))
+
+        expect(transduce(transducer, arrayReducer, initialValue, collection)).toEqual([6,8,10])
     })
 });
  
